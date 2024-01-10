5aa7e21bf736c41e3e58c4239105eeaf5066b60e
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-10 12:45:37 +0530

    using function

---
+++ 09_colorChanger/script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+console.log(body);
+
+function red() {
+    console.log('red is working');
+    body.style.backgroundColor = 'red'
+    // alert('Red Color')
+}
+function green() {
+    console.log('green is working');
+    body.style.backgroundColor = 'green'
+    
+}
+function blue() {
+    console.log('blue is working');
+    body.style.backgroundColor = 'blue'
+    
+}
+function yellow() {
+    console.log('yellow is working');
+    body.style.backgroundColor = 'yellow'
+    
+}
