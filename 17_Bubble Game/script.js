d0522cf7bd26bf951f365182578336c970222ef2
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-29 16:54:35 +0530

    bubble game

---
+++ 17_Bubble Game/script.js
@@ -0,0 +1,50 @@
+var timer = 30;
+var score = 0;
+var hitrn ;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector('#scoreVal').textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector('#hitVal').textContent = hitrn;
+}
+
+function makeBubble() {
+    var clutter = "";
+
+    for (var i = 1; i <= 133; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector('#timerval').textContent = timer;
+            // console.log(timer);
+        } else {
+            clearInterval(timerint);
+            document.querySelector('#pbtm').innerHTML = `<h1>Game Over</h1>`;
+        }
+    }, 1000)
+}
+
+document.querySelector('#pbtm').addEventListener("click", function (detail){
+    var clickedNum = Number(detail.target.textContent)
+    if(clickedNum === hitrn){
+        increaseScore(); 
+        makeBubble();
+        getNewHit();
+    }
+});
+
+runTimer();
+makeBubble();
+getNewHit();
+// increaseScore();    
